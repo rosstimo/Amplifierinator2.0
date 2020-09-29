df468b4913091d14b8d732b06921b5368e50c9b1
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-09-29 15:59:55 -0600

    common emitter working

--- Amp/UniversalBiasCommonCollector.vb
+++ Amp/UniversalBiasCommonCollector.vb
@@ -1,150 +1,150 @@
-Public Class UniversalBiasCommonCollector
-    'Inherits UniversalBiased
-    Inherits Amplifier
+'Public Class UniversalBiasCommonCollector
+'    'Inherits UniversalBiased
+'    Inherits Amplifier
 
-    'Properties
-    Private _VCC As Integer
-    Public Overrides Property VCC As Integer
-        Get
-            Return _VCC
-        End Get
-        Set(ByVal value As Integer)
-            _VCC = value
-        End Set
-    End Property
+'    'Properties
+'    Private _VCC As Integer
+'    Public Overrides Property VCC As Integer
+'        Get
+'            Return _VCC
+'        End Get
+'        Set(ByVal value As Integer)
+'            _VCC = value
+'        End Set
+'    End Property
 
-    Private _R1 As Integer
-    Public Overrides Property R1 As Integer
-        Get
-            Return _R1
-        End Get
-        Set(ByVal value As Integer)
-            _R1 = value
-        End Set
-    End Property
+'    Private _R1 As Integer
+'    Public Overrides Property R1 As Integer
+'        Get
+'            Return _R1
+'        End Get
+'        Set(ByVal value As Integer)
+'            _R1 = value
+'        End Set
+'    End Property
 
-    Private _R2 As Integer
-    Public Overrides Property R2 As Integer
-        Get
-            Return _R2
-        End Get
-        Set(ByVal value As Integer)
-            _R2 = value
-        End Set
-    End Property
+'    Private _R2 As Integer
+'    Public Overrides Property R2 As Integer
+'        Get
+'            Return _R2
+'        End Get
+'        Set(ByVal value As Integer)
+'            _R2 = value
+'        End Set
+'    End Property
 
-    Private _RC As Integer
-    Public Overrides Property RC As Integer
-        Get
-            Return _RC
-        End Get
-        Set(ByVal value As Integer)
-            _RC = value
-        End Set
-    End Property
+'    Private _RC As Integer
+'    Public Overrides Property RC As Integer
+'        Get
+'            Return _RC
+'        End Get
+'        Set(ByVal value As Integer)
+'            _RC = value
+'        End Set
+'    End Property
 
-    Private _RE As Integer
-    Public Overrides Property RE As Integer
-        Get
-            Return _RE
-        End Get
-        Set(ByVal value As Integer)
-            _RE = value
-        End Set
-    End Property
+'    Private _RE As Integer
+'    Public Overrides Property RE As Integer
+'        Get
+'            Return _RE
+'        End Get
+'        Set(ByVal value As Integer)
+'            _RE = value
+'        End Set
+'    End Property
 
-    Private _beta As Integer
-    Public Overrides Property beta As Integer
-        Get
-            Return _beta
-        End Get
-        Set(ByVal value As Integer)
-            _beta = value
-        End Set
-    End Property
+'    Private _beta As Integer
+'    Public Overrides Property beta As Integer
+'        Get
+'            Return _beta
+'        End Get
+'        Set(ByVal value As Integer)
+'            _beta = value
+'        End Set
+'    End Property
 
-    Private _IsNPN As Boolean
-    Public Property isNPN As Boolean
-        Get
-            Return _IsNPN
-        End Get
-        Set(ByVal value As Boolean)
-            _IsNPN = value
-        End Set
-    End Property
+'    Private _IsNPN As Boolean
+'    Public Property isNPN As Boolean
+'        Get
+'            Return _IsNPN
+'        End Get
+'        Set(ByVal value As Boolean)
+'            _IsNPN = value
+'        End Set
+'    End Property
 
-    Private _rLoadAC As Decimal
-    Public Overrides Property rLoadAC() As Decimal
-        Get
-            Return rLoadACCC()
-        End Get
-        Set(ByVal value As Decimal)
-            _rLoadAC = value
-        End Set
-    End Property
+'    Private _rLoadAC As Decimal
+'    Public Overrides Property rLoadAC() As Decimal
+'        Get
+'            Return rLoadACCC()
+'        End Get
+'        Set(ByVal value As Decimal)
+'            _rLoadAC = value
+'        End Set
+'    End Property
 
-    Private _zout As Decimal
-    Public Overrides Property zout() As Decimal
-        Get
-            Return ZoutCC()
-        End Get
-        Set(ByVal value As Decimal)
-            _zout = value
-        End Set
-    End Property
+'    Private _zout As Decimal
+'    Public Overrides Property zout() As Decimal
+'        Get
+'            Return ZoutCC()
+'        End Get
+'        Set(ByVal value As Decimal)
+'            _zout = value
+'        End Set
+'    End Property
 
-    Private _zin As Decimal
-    Public Overrides Property zin() As Decimal
-        Get
-            Return Me.ZinCC '_zin
-        End Get
-        Set(ByVal value As Decimal)
-            _zin = value
-        End Set
-    End Property
-    Private _vin As Decimal
-    Public Overrides Property vin() As Decimal
-        Get
-            Return vinCC()
-        End Get
-        Set(ByVal value As Decimal)
-            _vin = value
-        End Set
-    End Property
-    Private _vout As Decimal
-    Public Overrides Property vout() As Decimal
-        Get
-            Return voutCC()
-        End Get
-        Set(ByVal value As Decimal)
-            _vout = value
-        End Set
-    End Property
+'    Private _zin As Decimal
+'    Public Overrides Property zin() As Decimal
+'        Get
+'            Return Me.ZinCC '_zin
+'        End Get
+'        Set(ByVal value As Decimal)
+'            _zin = value
+'        End Set
+'    End Property
+'    Private _vin As Decimal
+'    Public Overrides Property vin() As Decimal
+'        Get
+'            Return vinCC()
+'        End Get
+'        Set(ByVal value As Decimal)
+'            _vin = value
+'        End Set
+'    End Property
+'    Private _vout As Decimal
+'    Public Overrides Property vout() As Decimal
+'        Get
+'            Return voutCC()
+'        End Get
+'        Set(ByVal value As Decimal)
+'            _vout = value
+'        End Set
+'    End Property
 
-    Sub New()
+'    Sub New()
 
-    End Sub
+'    End Sub
 
-    Public Function ZinCC() As Decimal 'CC
-        Return CDec((Me.R1 ^ -1 + Me.R2 ^ -1 + ((Me.beta + 1) * (Me.rPrimeE + (Me.RE ^ -1 + Me.RL ^ -1) ^ -1) ^ -1)) ^ -1)
-    End Function
+'    Public Function ZinCC() As Decimal 'CC
+'        Return CDec((Me.R1 ^ -1 + Me.R2 ^ -1 + ((Me.beta + 1) * (Me.rPrimeE + (Me.RE ^ -1 + Me.RL ^ -1) ^ -1) ^ -1)) ^ -1)
+'    End Function
 
-    Public Function ZoutCC() As Decimal 'CC
-        Return (Me.RE ^ -1 + (Me.rPrimeE + (((Me.rgen ^ -1 + R1 ^ -1 + R2 ^ -1) ^ -1) / (Me.beta + 1))) ^ -1) ^ -1
-    End Function
+'    Public Function ZoutCC() As Decimal 'CC
+'        Return (Me.RE ^ -1 + (Me.rPrimeE + (((Me.rgen ^ -1 + R1 ^ -1 + R2 ^ -1) ^ -1) / (Me.beta + 1))) ^ -1) ^ -1
+'    End Function
 
-    Public Function vinCC() As Decimal 'CC
-        Return Me.IE * (Me.rPrimeE + (Me.RE ^ -1 + Me.RL ^ -1) ^ -1)
-    End Function
+'    Public Function vinCC() As Decimal 'CC
+'        Return Me.IE * (Me.rPrimeE + (Me.RE ^ -1 + Me.RL ^ -1) ^ -1)
+'    End Function
 
-    Public Function voutCC() As Decimal 'CC
-        Return CDec(Me.IE * (Me.RE ^ -1 + Me.RL ^ -1) ^ -1)
-    End Function
+'    Public Function voutCC() As Decimal 'CC
+'        Return CDec(Me.IE * (Me.RE ^ -1 + Me.RL ^ -1) ^ -1)
+'    End Function
 
-    Public Function rLoadACCC() As Decimal 'CC
-        Return CDec((Me.RE ^ -1 + Me.RL ^ -1) ^ -1)
-    End Function
+'    Public Function rLoadACCC() As Decimal 'CC
+'        Return CDec((Me.RE ^ -1 + Me.RL ^ -1) ^ -1)
+'    End Function
 
 
 
-End Class
+'End Class
--- Amp/UniversalBiasCommonEmitter.vb
+++ Amp/UniversalBiasCommonEmitter.vb
@@ -1,56 +1,5 @@
 Public Class UniversalBiasCommonEmitter
-    'Inherits Amplifier
-    'Implements ICommonEmitter
 
-    'Private _VCC As Integer
-    'Public Property VCC As Integer
-    '    Get
-    '        Return _VCC
-    '    End Get
-    '    Set(ByVal value As Integer)
-    '        _VCC = value
-    '    End Set
-    'End Property
-
-    'Private _R1 As Integer
-    'Public Property R1 As Integer
-    '    Get
-    '        Return _R1
-    '    End Get
-    '    Set(ByVal value As Integer)
-    '        _R1 = value
-    '    End Set
-    'End Property
-
-    'Private _R2 As Integer
-    'Public Property R2 As Integer
-    '    Get
-    '        Return _R2
-    '    End Get
-    '    Set(ByVal value As Integer)
-    '        _R2 = value
-    '    End Set
-    'End Property
-
-    'Private _RC As Integer
-    'Public Property RC As Integer
-    '    Get
-    '        Return _RC
-    '    End Get
-    '    Set(ByVal value As Integer)
-    '        _RC = value
-    '    End Set
-    'End Property
-
-    'Private _RE As Integer
-    'Public Property RE As Integer
-    '    Get
-    '        Return _RE
-    '    End Get
-    '    Set(ByVal value As Integer)
-    '        _RE = value
-    '    End Set
-    'End Property
 
     'Private _beta As Integer
     'Public Property beta As Integer
@@ -63,57 +12,87 @@ Public Class UniversalBiasCommonEmitter
     'End Property
 
 
-    Private _rLoadAC As Decimal
-    Public Property rLoadAC() As Decimal
+    'Private _rLoadAC As Decimal
+    'Public Property rLoadAC() As Decimal
+    '    Get
+    '        Return _rLoadAC
+    '    End Get
+    '    Set(ByVal value As Decimal)
+    '        _rLoadAC = value
+    '    End Set
+    'End Property
+
+    'Private _rPrimeE As Decimal
+    'Public Property rPrimeE() As Decimal
+    '    Get
+    '        Return _rPrimeE
+    '    End Get
+    '    Set(ByVal value As Decimal)
+    '        _rPrimeE = value
+    '    End Set
+    'End Property
+
+    Private _rSwamp As Decimal
+    Public Property rSwamp() As Decimal
         Get
-            Return rLoadACCE()
+            Return _rSwamp
         End Get
         Set(ByVal value As Decimal)
-            _rLoadAC = value
+            _rSwamp = value
         End Set
     End Property
 
-    'Private _zout As Decimal
-    'Public Property zout() As Decimal
-    '    Get
-    '        Return ZoutCE()
-    '    End Get
-    '    Set(ByVal value As Decimal)
-    '        _zout = value
-    '    End Set
-    'End Property
-    'Private _zin As Decimal
-    'Public Property zin() As Decimal
-    '    Get
-    '        Return Me.zinCE '_zin
-    '    End Get
-    '    Set(ByVal value As Decimal)
-    '        _zin = value
-    '    End Set
-    'End Property
-    'Private _vin As Decimal
-    'Public Property vin() As Decimal
-    '    Get
-    '        Return vinCE()
-    '    End Get
-    '    Set(ByVal value As Decimal)
-    '        _vin = value
-    '    End Set
-    'End Property
-    'Private _vout As Decimal
-    'Public Property vout() As Decimal
-    '    Get
-    '        Return voutCE()
-    '    End Get
-    '    Set(ByVal value As Decimal)
-    '        _vout = value
-    '    End Set
-    'End Property
+    Private _RL As Decimal
+    Public Property RL() As Decimal
+        Get
+            Return _RL
+        End Get
+        Set(ByVal value As Decimal)
+            _RL = value
+        End Set
+    End Property
 
-    Public Property RL As Integer
+    Private _rgen As Decimal
+    Public Property rgen() As Decimal
+        Get
+            Return _rgen
+        End Get
+        Set(ByVal value As Decimal)
+            _rgen = value
+        End Set
+    End Property
+
+    Private _cIn As Decimal
+    Public Property cIn() As Decimal
+        Get
+            Return _cIn
+        End Get
+        Set(ByVal value As Decimal)
+            _cIn = value
+        End Set
+    End Property
+
+    Private _cOut As Decimal
+    Public Property cOut() As Decimal
+        Get
+            Return _cOut
+        End Get
+        Set(ByVal value As Decimal)
+            _cOut = value
+        End Set
+    End Property
+
+    Private _cBypass As Decimal
+    Public Property cBypass() As Decimal
+        Get
+            Return _cBypass
+        End Get
+        Set(ByVal value As Decimal)
+            _cBypass = value
+        End Set
+    End Property
 
     Public Universalbias As New UniversalBiased
-    ' 
 
     Sub New(Optional ByVal VCC% = 30%,
             Optional ByVal R1% = 120000%,
@@ -128,6 +107,7 @@ Public Class UniversalBiasCommonEmitter
             Optional ByVal cOut# = 1 * 10 ^ -6,
             Optional ByVal cBypass# = 1 * 10 ^ -6
             )
+
         Me.Universalbias.VCC = VCC
         Me.Universalbias.R1 = R1
         Me.Universalbias.R2 = R2
@@ -136,49 +116,124 @@ Public Class UniversalBiasCommonEmitter
         Me.Universalbias.beta = beta
 
 
+        Me.rSwamp = rSwamp
+        Me.RL = RL
+        Me.rgen = rgen
+        Me.cIn = cIn
+        Me.cOut = cOut
+        Me.cBypass = cBypass
+
 
 
         Console.WriteLine("CE")
         'Me.universalbias.beta = 
     End Sub
 
-    Public Function rLoadACCE() As Decimal
+    Public Function rLoadAC() As Decimal
         Return CDec((Me.Universalbias.RC ^ -1 + Me.RL ^ -1) ^ -1)
     End Function
 
-    'Private Function zinCE() As Decimal
-    '    Return CDec((Me.R1 ^ -1 + Me.R2 ^ -1 + ((Me.beta + 1) * (Me.rPrimeE + Me.rSwamp)) ^ -1) ^ -1)
-    'End Function
+    Public Function zin() As Decimal
+        Return CDec((Me.Universalbias.R1 ^ -1 + Me.Universalbias.R2 ^ -1 + ((Me.Universalbias.beta + 1) * (Me.rPrimeE + Me.rSwamp)) ^ -1) ^ -1)
+    End Function
 
-    'Public Function ZoutCE() As Decimal
-    '    Return Me.RC
-    'End Function
+    Public Function Zout() As Decimal
+        Return Me.Universalbias.RC
+    End Function
 
-    'Public Function voutCE() As Decimal 'max TODO rename?? deltaIB ??
-    '    Return CDec(Me.Universalbias.IC * Me.rLoadAC)
-    'End Function
+    Public Function rPrimeE() As Decimal
+        Return 0.026D / Me.Universalbias.IE
+    End Function
 
-    'Public Function vinCE() As Decimal
-    '    Return Me.Universalbias.IE * (Me.rPrimeE + Me.rSwamp)
-    'End Function
+    Public Function vin() As Decimal
+        Return Me.Universalbias.IE * (Me.rPrimeE + Me.rSwamp)
+    End Function
 
-    'Public Function rthcbypass() As Double
-    '    Return (Me.rEBypass ^ -1 + (Me.rSwamp + Me.rPrimeE + ((Me.R1 ^ -1 + Me.R2 ^ -1 + Me.rgen ^ -1) ^ -1 / (Me.beta + 1))) ^ -1) ^ -1
-    'End Function
+    Public Function vout() As Decimal
+        Return CDec(Me.Universalbias.IC * Me.rLoadAC)
+    End Function
 
-    'Public Function fcbypass() As Decimal
-    '    Return LibElectronicsMath.CriticalFrequency(Me.rthcbypass, Me.cBypass)
-    'End Function
+    Public Function vceCutOffAC() As Decimal
+        Return Me.vout + Me.Universalbias.VCE
+    End Function
 
-    'Public numberOfCapacitors As Integer = 3I
+    Public Function Av() As Decimal
+        Return Me.vout / Me.vin 'CE, CB, CC
+    End Function
+    Public Function Ai() As Decimal 'CE, CB, CC
+        Return Me.Av * (Me.zin() / Me.RL)
+    End Function
+    Public Function Ap() As Decimal 'CE, CB, CC
+        Return Me.Av * Me.Ai
+    End Function
+    Public Function AvdB() As Decimal 'CE, CB, CC
+        Return CDec(20 * Math.Log10(Me.Av))
+    End Function
+    Public Function AidB() As Decimal 'CE, CB, CC
+        Return CDec(20 * Math.Log10(Me.Ai))
+    End Function
+    Public Function ApdB() As Decimal 'CE, CB, CC
+        Return CDec(10 * Math.Log10(Me.Ap))
+    End Function
+
+    'Any Bias
+    'CE, CB Only
+    Public Function rEBypass() As Decimal 'CE, CB
+        Return CDec(Me.Universalbias.RE - Me.rSwamp)
+    End Function
+
+    Public Function icSatAC() As Decimal 'CE, CB
+        Return Me.Universalbias.IC + (Me.Universalbias.VCE / Me.rLoadAC)
+    End Function
+
+    'Frequency Response
+    Public Function RthCIn() As Double 'CE, CB, CC
+        Return (Me.rgen ^ -1 + Me.zin ^ -1) ^ -1
+    End Function
+
+    Public Function RthCOut() As Double 'CE, CB, CC
+        Return Me.Zout + Me.RL
+    End Function
+
+    Public Function Fcin() As Decimal
+        Return LibElectronicsMath.CriticalFrequency(Me.RthCIn, Me.cIn)
+    End Function
+
+    Public Function FcOut() As Decimal
+        Return LibElectronicsMath.CriticalFrequency(Me.RthCOut, Me.cOut)
+    End Function
+
+    Public fcLowTargetDefault As Decimal = 100D
 
 
-    'Public Function lowestcbypass() As Double
-    '    Return LibElectronicsMath.Capacitance(Me.rthcbypass, Me.fcLowTargetOffset(Me.numberOfCapacitors, Me.fcLowTargetDefault))
-    'End Function
+    Public Function fcLowTargetOffset(ByVal numberOfCapacitors As Integer, ByVal fcLowTarget As Decimal) As Double
+        Return (fcLowTarget / numberOfCapacitors) * Math.Sqrt(numberOfCapacitors)
+    End Function
 
-    'Public Function fclow() As Decimal
-    '    Return CDec(Math.Sqrt(Me.Fcin ^ 2 + Me.FcOut ^ 2 + Me.fcbypass ^ 2))
-    'End Function
+    Public Function LowestCIn() As Double
+        Return LibElectronicsMath.Capacitance(Me.RthCIn, fcLowTargetOffset(numberOfCapacitors, fcLowTargetDefault))
+    End Function
+
+    Public Function LowestCOut() As Double
+        Return LibElectronicsMath.Capacitance(Me.RthCOut, fcLowTargetOffset(numberOfCapacitors, fcLowTargetDefault))
+    End Function
+
+    Public Function rthcbypass() As Double
+        Return (Me.rEBypass ^ -1 + (Me.rSwamp + Me.rPrimeE + ((Me.Universalbias.R1 ^ -1 + Me.Universalbias.R2 ^ -1 + Me.rgen ^ -1) ^ -1 / (Me.Universalbias.beta + 1))) ^ -1) ^ -1
+    End Function
+
+    Public Function fcbypass() As Decimal
+        Return LibElectronicsMath.CriticalFrequency(Me.rthcbypass, Me.cBypass)
+    End Function
+
+    Public numberOfCapacitors As Integer = 3I
+
+    Public Function lowestcbypass() As Double
+        Return LibElectronicsMath.Capacitance(Me.rthcbypass, Me.fcLowTargetOffset(Me.numberOfCapacitors, Me.fcLowTargetDefault))
+    End Function
+
+    Public Function fclow() As Decimal
+        Return CDec(Math.Sqrt(Me.Fcin ^ 2 + Me.FcOut ^ 2 + Me.fcbypass ^ 2))
+    End Function
 
 End Class
